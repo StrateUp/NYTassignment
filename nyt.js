27f9ad4e43b03175dffdd457fba12a9d43ca0308
Greg Lee <gml2136@gmail.com>
2017-02-18 15:54:10 -0800

    added the done logic to the already existing nyt.js file, cleaned it up, and realized that the apikey we were given doesn't work - working on getting a new one

--- nyt.js
+++ nyt.js
@@ -2,9 +2,10 @@
 
 var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
-  authKey + "&q=";
+  authKey;
 
-$("#nySearch").submit(function(){
+$("#nySearch").submit(function(event){
+  event.preventDefault();
   var searchTerm = $("#nySearch > #searchTerm").val();
   var numRecords = $("#nySearch > #records").val();
   var startYear = $("#nySearch > #startYear").val();
@@ -12,6 +13,55 @@ $("#nySearch").submit(function(){
   queryURLBase += $.param({q: searchTerm, fl: numRecords, begin_date: startYear, end_date: endYear});
 
   $.ajax({url: queryURLBase, method:"GET"}).done((response) => {
-    console.log(response);
-  });
+
+    // ========= articleDiv
+
+    var articleDiv = $("<div>");
+
+    // =========headline
+
+    var headline = response.docs.headline.main;
+
+    var headlineP = $("<h1>").text(headline);
+
+    articleDiv.append(headlineP);
+
+    // =========author
+
+    var author = response.docs.multimedia.credit;
+
+    var authorP = $("<p>").text("By" +author);
+
+    articleDiv.append(authorP);
+
+    // ==========section
+
+    var section = response.docs.source;
+
+    var sectionP = $("<p>").text("Section:" +section)
+
+    articleDiv.append(sectionP);
+
+    // ==========date
+
+    var date = response.docs.pub_date;
+
+    var dateP = $("<p>").text(date);
+
+    articleDiv.append(dateP);
+
+    // ===========url
+
+    var url = response.docs.headline.main;
+
+    var urlP = $('<a>').text(url);
+
+    articleDiv.append(url);
+
+    // =========over all attachment to HTML
+
+    $('#articles').append(articleDiv);
+
+
+    });
 })
